23e2e8ab436f118d13453d3c45ba75a3227fdc3c
prayasincloud <107023940+prayasincloud@users.noreply.github.com>
2022-12-14 11:06:06 +0530

    Update main.py

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -3,7 +3,7 @@ import datetime as dt
 import random
 
 MY_EMAIL = "prayaspatel199@gmail.com"
-MY_PASSWORD = "hmawthngmqnhymkn"
+MY_PASSWORD = ""
 
 now = dt.datetime.now()
 day_of_week = now.weekday()
@@ -17,29 +17,3 @@ if day_of_week == 2:
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(from_addr=MY_EMAIL, to_addrs="prayaspatel199@gmail.com", msg=f"quote\n{quote}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-# import smtplib
-#
-# my_email = "prayaspatel199@gmail.com "
-# password = "hmawthngmqnhymkn"
-#
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="prayaspatel199@gmail.com",
-#         msg="Subject:Hello\n\n\n Hi this is how you write body")
-#
