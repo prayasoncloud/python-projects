f78a7a7f5ba3e5ee2b97093482c684310915ee25
Prayas <prayaspatel199@gmail.com>
2024-09-08 11:14:13 +0530

    updaitng files

--- rain_detector/main.py
+++ rain_detector/main.py
@@ -11,11 +11,25 @@ parameters = {
     "lon": MY_LONG,
     "appid": key,
     "cnt": 3
-
+    
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
 data = response.json()
 
-print(data)
+data_id = data["list"]
+
+will_rain = True
+
+for hours in data_id:
+    code = hours["weather"][0]["id"]
+    if code < 700:
+        will_rain = False
+
+if will_rain == False:
+    print("Bring an Umbrella")
+
+
+
+
